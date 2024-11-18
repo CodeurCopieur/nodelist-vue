83d5458bb0e629b3d8e42baf005fef2d49500ea2
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-11-19 00:40:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <div>
+      <RouterLink :to="{ name: 'notes' }">Notes</RouterLink> |
+      <RouterLink :to="{ name: 'stats' }">Stats</RouterLink>
+    </div>
+    <RouterView />
+  </div>
+</template>
---
+++ src/Views/NotesView.vue
@@ -0,0 +1,13 @@
+<script setup>
+  
+</script>
+
+<template>
+  <div>
+    <h1>Notes</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
