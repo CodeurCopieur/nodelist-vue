4b496743c9b7a9389e5f909d3b4077a55a141f2a
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-11-22 00:13:16 +0100

    Rendre le NavBar mobile réactif et ajouter RouterLink

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -1,14 +1,23 @@
 <script setup>
-  
+import { ref } from 'vue';
+
+  const showNavBarMenu = ref(false);
 </script>
 
 <template>
 
  <nav class="navbar is-danger" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item is-size-4 is-family-monospace" href="#">NotesList</a>
+    <RouterLink class="navbar-item is-size-4 is-family-monospace" 
+    :to="{ name: 'notes' }" active-class="is-active">NotesList</RouterLink>
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a role="button"
+      :class="{ 'is-active' : showNavBarMenu}" 
+      @click.prevent="showNavBarMenu = !showNavBarMenu"
+       class="navbar-burger" 
+       aria-label="menu" 
+       aria-expanded="false" 
+       data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
@@ -16,24 +25,15 @@
     </a>
   </div>
 
-  <div id="navbarBasicExample" class="navbar-menu">
-    <!-- <div class="navbar-start">
-      <RouterLink :to="{ name: 'notes' }" class="navbar-item">
-        Notes
-      </RouterLink>
-
-      <RouterLink :to="{ name: 'stats' }" class="navbar-item">
-        Stats
-      </RouterLink>
-
-    </div> -->
-
+  <div id="navbarBasicExample" 
+    class="navbar-menu"
+    :class="{ 'is-active' : showNavBarMenu}">
     <div class="navbar-end">
-      <RouterLink :to="{ name: 'notes' }" class="navbar-item">
+      <RouterLink :to="{ name: 'notes' }" active-class="is-active" class="navbar-item">
         Notes
       </RouterLink>
 
-      <RouterLink :to="{ name: 'stats' }" class="navbar-item">
+      <RouterLink :to="{ name: 'stats' }" active-class="is-active" class="navbar-item">
         Stats
       </RouterLink>
     </div>
@@ -41,6 +41,16 @@
 </nav>
 </template>
 
-<style lang="scss" scoped>
-  
+<style scoped>
+  @media (max-width: 1023px) {
+    .navbar-menu {
+      position: absolute;
+      left: 0;
+      right: 0;
+    }
+  }
+
+  .router-link-exact-active, .navbar-burger {
+    color: #fff;
+  }
 </style>
