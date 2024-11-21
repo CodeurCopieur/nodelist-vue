d1503df4f7f8974a59f7f8cd0611952db60addcf
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-11-22 00:36:31 +0100

    indentation html

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -5,40 +5,41 @@ import { ref } from 'vue';
 </script>
 
 <template>
+  <nav class="navbar is-danger" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <RouterLink class="navbar-item is-size-4 is-family-monospace" 
+        :to="{ name: 'notes' }" active-class="is-active">NotesList</RouterLink>
 
- <nav class="navbar is-danger" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <RouterLink class="navbar-item is-size-4 is-family-monospace" 
-    :to="{ name: 'notes' }" active-class="is-active">NotesList</RouterLink>
+        <a role="button"
+          :class="{ 'is-active' : showNavBarMenu}" 
+          @click.prevent="showNavBarMenu = !showNavBarMenu"
+          class="navbar-burger" 
+          aria-label="menu" 
+          aria-expanded="false" 
+          data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
-    <a role="button"
-      :class="{ 'is-active' : showNavBarMenu}" 
-      @click.prevent="showNavBarMenu = !showNavBarMenu"
-       class="navbar-burger" 
-       aria-label="menu" 
-       aria-expanded="false" 
-       data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
+      <div id="navbarBasicExample" 
+        class="navbar-menu"
+        :class="{ 'is-active' : showNavBarMenu}">
+        <div class="navbar-end">
+          <RouterLink :to="{ name: 'notes' }" active-class="is-active" class="navbar-item">
+            Notes
+          </RouterLink>
 
-  <div id="navbarBasicExample" 
-    class="navbar-menu"
-    :class="{ 'is-active' : showNavBarMenu}">
-    <div class="navbar-end">
-      <RouterLink :to="{ name: 'notes' }" active-class="is-active" class="navbar-item">
-        Notes
-      </RouterLink>
-
-      <RouterLink :to="{ name: 'stats' }" active-class="is-active" class="navbar-item">
-        Stats
-      </RouterLink>
-    </div>
-  </div>
-</nav>
+          <RouterLink :to="{ name: 'stats' }" active-class="is-active" class="navbar-item">
+            Stats
+          </RouterLink>
+        </div>
+      </div>
+    </div>  
+  </nav>
 </template>
 
 <style scoped>
