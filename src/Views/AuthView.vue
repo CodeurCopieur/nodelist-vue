38463541d28ee1bd32d52858d0ab50074a1c0201
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2025-05-23 19:18:12 +0200

    Concevoir la page de connexion Auth à l'aide de Bulma dans API de composition Vue

---
+++ src/Views/AuthView.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="auth-form">
+    <div class="tabs is-centered">
+        <ul>
+            <li :class="{ 'is-active': !register }">
+                <a @click="register = false">Login</a>
+            </li>
+            <li :class="{ 'is-active': register }">
+                <a @click="register = true">Register</a>
+            </li>
+        </ul>
+    </div>
+
+    <form class="card auth">
+        <div class="card-content">
+            <div class="title">
+                {{ register ? 'Register' : 'Login' }}
+            </div>  
+            <div class="content">
+                <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control">
+                        <input class="input" type="email" placeholder="Email">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Password</label>
+                    <div class="control">
+                        <input class="input" type="password" placeholder="Password">
+                    </div>
+                </div>  
+                <div class="field">
+                    <div class="control">
+                        <button class="button is-danger" type="submit">
+                            {{ register ? 'Register' : 'Login' }}
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </form>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const register = ref(false);
+</script>
+<style scoped>
+    .auth {
+        width: 400px;
+        margin: 0 auto;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import NotesView from '../Views/NotesView.vue'
 import StatsView from '../Views/StatsView.vue'
 import EditNoteView from '../Views/EditNoteView.vue'
+import AuthView from '../Views/AuthView.vue'
 
 const routes = [
   { path: '/', component: NotesView, name: 'notes' },
   { path: '/edit-note/:id', component: EditNoteView, name: 'edit-note' },
   { path: '/stats', component: StatsView, name: 'stats' },
+  { path: '/auth', component: AuthView, name: 'auth' },
 ]
 
 const router = createRouter({
